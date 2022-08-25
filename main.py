8bf9f3750c1c834d3cfbd0a42112fffd40180618
Marvish Subhas Chandra <mschandr@uci.edu>
2022-08-25 16:27:50 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+
+
+class oneStory:
+    def __init__(self):
+        self.squareFoot = squareFoot
+
+    def floors():
+    def specializedWorkers():
+    def expenses(buildingPermit,electricalPermit,plumbingPermit,hvacPermit)
+    def foundationRequirements()
+    def plumbingRequirements()
+    def homeFrame()
+    def sheathingNeeds()
+    def windowsDoors()
+    def hvacSystem()
+    def electricSystem()
+    def roofInstallation()
+    def homeInsulation()
+
+class twoStory:
+class threeStory:
+class pentHouse:
+class mansionHome:
